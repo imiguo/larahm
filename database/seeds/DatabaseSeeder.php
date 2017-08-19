b92e5619d9cceaa4b6a030cab82fd64ec291fba7
entimm <entimm@gmail.com>
2017-08-19 12:33:56 +0800

    Update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->truncate();
+        User::create([
+            'name' => 'admin',
+            'username' => 'miadmin',
+            'password' => bcrypt('miadmin'),
+            'date_register' => Carbon::now(),
+            'email' => 'midollaradm@gmail.com',
+        ]);
+        User::create([
+            'name' => 'test',
+            'username' => 'test',
+            'password' => bcrypt('test'),
+            'date_register' => Carbon::now(),
+            'email' => '1194316669@qq.com',
+        ]);
+
         $file = __DIR__.'/import.sql';
         DB::unprepared(File::get($file));
     }
