056d8eab640c81154ecce2144fdf6a90d352dce3
entimm <entimm@gmail.com>
2017-08-23 22:18:22 +0800

    Update

--- app/Hm/http/payments/payeer.php
+++ app/Hm/http/payments/payeer.php
@@ -15,56 +15,54 @@ use App\Models\Order;
 file_put_contents('../log/payeer_processing_'.env('APP_ENV').'.txt', json_encode(app('data')->frm).PHP_EOL, FILE_APPEND);
 file_put_contents('../log/payeer_processing_'.env('APP_ENV').'.txt', 'IP:'.app('data')->env['REMOTE_ADDR'].PHP_EOL, FILE_APPEND);
 
-if (app('data')->frm['a'] == 'checkpayment') {
-    // Rejecting queries from IP addresses not belonging to Payeer
-    if (! in_array($_SERVER['REMOTE_ADDR'], ['185.71.65.92', '185.71.65.189',
-        '149.202.17.210', ])) {
-        throw new EmptyException();
-    }
-    if (isset($_POST['m_operation_id']) && isset($_POST['m_sign'])) {
-        $m_secret_key = config('payeer.shop_secret_key');
-        // Forming an array for signature generation
-        $arHash = [
-            $_POST['m_operation_id'],
-            $_POST['m_operation_ps'],
-            $_POST['m_operation_date'],
-            $_POST['m_operation_pay_date'],
-            $_POST['m_shop'],
-            $_POST['m_orderid'],
-            $_POST['m_amount'],
-            $_POST['m_curr'],
-            $_POST['m_desc'],
-            $_POST['m_status'],
-        ];
-        // Adding additional parameters to the array if such parameters have been transferred
-        if (isset($_POST['m_params'])) {
-            $arHash[] = $_POST['m_params'];
-        }
-        // Adding the secret key to the array
-        $arHash[] = $m_secret_key;
-        // Forming a signature
-        $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
-        // If the signatures match and payment status is “Complete”
-        if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success') {
-            $orderNo = $_POST['m_orderid'];
-            $order = Order::where('order_no', $payment_id)->first();
-            $h_id = $order->data['plan_id'];
-            $compound = $order->data['compound'];
-            $user_id = $order->user_id;
-
-            add_deposit(2, $user_id, $_POST['m_amount'], $_POST['m_operation_id'], $_POST['client_account'], $h_id, $compound);
-            $order->status = Order::STATUS_OK;
-            $order->save();
-
-            // Here you can mark the invoice as paid or transfer funds to your customer
-            // Returning that the payment was processed successfully
-            echo $_POST['m_orderid'].'|success';
-            throw new EmptyException();
-        }
-        // If not, returning an error
-        echo $_POST['m_orderid'].'|error';
-        throw new EmptyException();
-    }
-
-    echo '1';
+// Rejecting queries from IP addresses not belonging to Payeer
+if (! in_array($_SERVER['REMOTE_ADDR'], ['185.71.65.92', '185.71.65.189',
+    '149.202.17.210', ])) {
+    throw new EmptyException();
 }
+if (isset($_POST['m_operation_id']) && isset($_POST['m_sign'])) {
+    $m_secret_key = config('payeer.shop_secret_key');
+    // Forming an array for signature generation
+    $arHash = [
+        $_POST['m_operation_id'],
+        $_POST['m_operation_ps'],
+        $_POST['m_operation_date'],
+        $_POST['m_operation_pay_date'],
+        $_POST['m_shop'],
+        $_POST['m_orderid'],
+        $_POST['m_amount'],
+        $_POST['m_curr'],
+        $_POST['m_desc'],
+        $_POST['m_status'],
+    ];
+    // Adding additional parameters to the array if such parameters have been transferred
+    if (isset($_POST['m_params'])) {
+        $arHash[] = $_POST['m_params'];
+    }
+    // Adding the secret key to the array
+    $arHash[] = $m_secret_key;
+    // Forming a signature
+    $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
+    // If the signatures match and payment status is “Complete”
+    if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success') {
+        $orderNo = $_POST['m_orderid'];
+        $order = Order::where('order_no', $payment_id)->first();
+        $h_id = $order->data['plan_id'];
+        $compound = $order->data['compound'];
+        $user_id = $order->user_id;
+
+        add_deposit(2, $user_id, $_POST['m_amount'], $_POST['m_operation_id'], $_POST['client_account'], $h_id, $compound);
+        $order->status = Order::STATUS_OK;
+        $order->save();
+
+        // Here you can mark the invoice as paid or transfer funds to your customer
+        // Returning that the payment was processed successfully
+        echo $_POST['m_orderid'].'|success';
+        throw new EmptyException();
+    }
+    // If not, returning an error
+    echo $_POST['m_orderid'].'|error';
+    throw new EmptyException();
+}
+
+echo '1';
--- app/Hm/http/payments/perfectmoney.php
+++ app/Hm/http/payments/perfectmoney.php
@@ -9,87 +9,41 @@
  * with this source code in the file LICENSE.
  */
 
-use App\Exceptions\EmptyException;
 use App\Models\Order;
 
 file_put_contents('../log/perfectmoney_processing_'.env('APP_ENV').'.txt', json_encode(app('data')->frm).PHP_EOL, FILE_APPEND);
 file_put_contents('../log/perfectmoney_processing_'.env('APP_ENV').'.txt', 'IP:'.app('data')->env['REMOTE_ADDR'].PHP_EOL, FILE_APPEND);
 
 $mymd5 = app('data')->settings['md5altphrase_perfectmoney'];
-if (app('data')->frm['a'] == 'pay_withdraw') {
+
+$string = app('data')->frm['PAYMENT_ID'].':'.app('data')->frm['PAYEE_ACCOUNT'].':'.
+          app('data')->frm['PAYMENT_AMOUNT'].':'.app('data')->frm['PAYMENT_UNITS'].':'.
+          app('data')->frm['PAYMENT_BATCH_NUM'].':'.
+          app('data')->frm['PAYER_ACCOUNT'].':'.$mymd5.':'.
+          app('data')->frm['TIMESTAMPGMT'];
+$hash = strtoupper(md5($string));
+
+if ($hash == app('data')->frm['V2_HASH']) {
+    // $ip = app('data')->env['REMOTE_ADDR'];
+    // if ( ! preg_match('/63\\.240\\.230\\.\\d/i', $ip)) {
+    //     throw new EmptyException();
+    // }
+
+    $payment_id = sprintf('%d', app('data')->frm['PAYMENT_ID']);
+
+    $order = Order::where('order_no', $payment_id)->first();
+    $h_id = $order->data['plan_id'];
+    $compound = $order->data['compound'];
+    $user_id = $order->user_id;
+
+    $amount = app('data')->frm['PAYMENT_AMOUNT'];
     $batch = app('data')->frm['PAYMENT_BATCH_NUM'];
-    list($id, $str) = explode('-', app('data')->frm['withdraw']);
-    $id = sprintf('%d', $id);
-    if ($str == '') {
-        $str = 'abcdef';
+    $account = app('data')->frm['PAYER_ACCOUNT'];
+    if (app('data')->frm['PAYMENT_UNITS'] == 'USD') {
+        add_deposit(1, $user_id, $amount, $batch, $account, $h_id, $compound);
+        $order->status = Order::STATUS_OK;
+        $order->save();
     }
-
-    $str = quote($str);
-    $q = 'select * from history where id = '.$id.' and str = \''.$str.'\' and type=\'withdraw_pending\'';
-    $sth = db_query($q);
-    while ($row = mysql_fetch_array($sth)) {
-        $q = 'delete from history where id = '.$id;
-        db_query($q);
-        $q = 'insert into history set
-	user_id = '.$row['user_id'].',
-	amount = -'.abs($row['amount']).(''.',
-	type = \'withdrawal\',
-	description = \'Withdraw processed. Batch id = '.$batch.'\',
-	actual_amount = -').abs($row['amount']).',
-	ec = 0,
-	date = now()
-	';
-        db_query($q);
-
-        $q = 'select * from users where id = '.$row['user_id'];
-        $usth = db_query($q);
-        $userinfo = mysql_fetch_array($usth);
-        $info = [$user];
-        $info['username'] = $userinfo['username'];
-        $info['name'] = $userinfo['name'];
-        $info['amount'] = sprintf('%.02f', abs($row['amount']));
-        $info['account'] = app('data')->frm['PAYEE_ACCOUNT'];
-        $info['batch'] = $batch;
-        $info['paying_batch'] = $batch;
-        $info['receiving_batch'] = $batch;
-        $info['currency'] = app('data')->exchange_systems[0]['name'];
-        send_template_mail('withdraw_user_notification', $userinfo['email'], app('data')->settings['system_email'], $info);
-    }
-
-    echo 1;
-    throw new EmptyException();
 }
 
-if (app('data')->frm['a'] == 'checkpayment') {
-    $string = app('data')->frm['PAYMENT_ID'].':'.app('data')->frm['PAYEE_ACCOUNT'].':'.
-              app('data')->frm['PAYMENT_AMOUNT'].':'.app('data')->frm['PAYMENT_UNITS'].':'.
-              app('data')->frm['PAYMENT_BATCH_NUM'].':'.
-              app('data')->frm['PAYER_ACCOUNT'].':'.$mymd5.':'.
-              app('data')->frm['TIMESTAMPGMT'];
-    $hash = strtoupper(md5($string));
-
-    if ($hash == app('data')->frm['V2_HASH']) {
-        // $ip = app('data')->env['REMOTE_ADDR'];
-        // if ( ! preg_match('/63\\.240\\.230\\.\\d/i', $ip)) {
-        //     throw new EmptyException();
-        // }
-
-        $payment_id = sprintf('%d', app('data')->frm['PAYMENT_ID']);
-
-        $order = Order::where('order_no', $payment_id)->first();
-        $h_id = $order->data['plan_id'];
-        $compound = $order->data['compound'];
-        $user_id = $order->user_id;
-
-        $amount = app('data')->frm['PAYMENT_AMOUNT'];
-        $batch = app('data')->frm['PAYMENT_BATCH_NUM'];
-        $account = app('data')->frm['PAYER_ACCOUNT'];
-        if (app('data')->frm['PAYMENT_UNITS'] == 'USD') {
-            add_deposit(1, $user_id, $amount, $batch, $account, $h_id, $compound);
-            $order->status = Order::STATUS_OK;
-            $order->save();
-        }
-    }
-
-    echo '1';
-}
+echo '1';
